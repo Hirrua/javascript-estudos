18c940bea21cce74bd0e9935efdb7404d8a1e695
Hirruá Souza <hirrua.silva20@gmail.com>
2024-02-13 18:31:24 -0300

    concatenação arrays

---
+++ arrays - avançado/concatenação de arrays/concatenacao.js
@@ -0,0 +1,8 @@
+const array01 = [1, 2, 3, 4, 5];
+const array02 = [6, 7, 8, 9, 10];
+
+// Utilizando o método concat (posso passar mais arrays)
+const array03 = array01.concat(array02);
+
+// Utilizando o spread operator (posso passar outros valores)
+const array04 = [...array01, ...array02]; 
