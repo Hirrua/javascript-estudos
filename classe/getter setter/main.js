a9b60d404d364b615de46325553c59131dbc8484
Hirrua <hirrua.silva20@gmail.com>
2024-05-04 15:38:03 -0300

    getter e setter

---
+++ classe/getter setter/main.js
@@ -0,0 +1,36 @@
+const _velocidade = Symbol('velocidade')
+
+class Carro {
+    constructor(nome) {
+        this.nome = nome;
+        this[_velocidade] = 0; // propriedade privada
+    }
+
+    get velocidade() {
+        console.log('GETTER');
+        return this[_velocidade];
+    }
+
+    set velocidade(valor) {
+        console.log('SETTER');
+        if(typeof valor !== 'number') return;
+        if(valor >= 100 || valor <= 0) return;
+
+        this[_velocidade] = valor;
+    }
+
+    acelerar() {
+        if(this[_velocidade] >= 100) return;
+        this[_velocidade]++;
+    }
+
+    freiar() {
+        if(this[_velocidade] <= 0) return;
+        this[_velocidade]--;
+    }
+}
+
+const c1 = new Carro('Palio');
+
+c1.velocidade = 55;
+console.log(c1.velocidade);
