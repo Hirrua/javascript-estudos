350c05cfe2917f6a4b3ef35a054c7da9044d81b8
Hirrua <hirrua.silva20@gmail.com>
2024-05-04 16:08:11 -0300

    herança

---
+++ classe/herança/main.js
@@ -0,0 +1,33 @@
+class Dipositivo {
+    constructor(marca) {
+        this.marca = marca;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if(this.ligado) {
+            console.log(`${this.marca} já está ligado`);
+            return;
+        }
+        this.ligado = true;
+    }
+
+    desligar() {
+        if(!this.ligado) {
+            console.log(`${this.marca} já está desligado`)
+        }
+        this.ligado = false;
+    }
+
+}
+
+class Smartphone extends Dipositivo {
+    constructor(marca, modelo, cor) {
+        super(marca); // recebe o marca que recebeu na classe pai (Dispositivo)
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+}
+
+const s1 = new Smartphone('IPhone', '15 Pro Max', 'Titanium natural');
+console.log(s1)
